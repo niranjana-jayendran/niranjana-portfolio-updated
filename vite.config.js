1e171e7d1e777a6566e9ed5d57fbd9e9224d076d
niranjana-jayendran <ninijai@outlook.com>
2025-11-01 20:37:55 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// If you use the special repo name `niranjana-jayendran.github.io`,
-// GitHub Pages serves from root, so base is '/'.
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/niranjana-portfolio-updated/',
 })
