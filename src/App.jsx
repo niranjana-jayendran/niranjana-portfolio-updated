bd4d58e5fee0f14053eda248e907e3036fefed89
niranjana-jayendran <ninijai@outlook.com>
2025-11-05 18:19:33 -0500

    testing image (3)

--- src/App.jsx
+++ src/App.jsx
@@ -669,7 +669,7 @@ const COMMUNITY = [
     ],
   },
   {
-    image: "/src/assets/bpa_waving.png",
+    image: waving
     title: "BPA Student Certification Series (Designer)",
     org: "BPA National / Chapters",
     details: [
